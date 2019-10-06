b3b930f93fe60d3c8d706955d6d5bb4bed3d05d0
Quentin Delfosse <q.delfosse@gmail.com>
2019-10-06 20:13:38 +0200

    loop asgard now handles gripper

--- loop_asgard.py
+++ loop_asgard.py
@@ -16,7 +16,7 @@ i = 0
 if s0.isOpen():
     print("")
     while True:
-        # First position
+        # First position     ##### FOR THE GREY ONE, DO NOT USE 4th one !
         angles = [20, 0, 0, 0, 0, 0]
         angles = [round(angles[0] / 6, 2), angles[1], angles[2], angles[3],
                 round(angles[4] / 13, 2), round(angles[5] / 3.33, 2)]
@@ -34,6 +34,20 @@ if s0.isOpen():
         message = "G0 " + "A" + str(angles[0]) + " B" + str(angles[1]) + " C" + str(angles[1]) + \
             " D" + str(angles[2]) + " X" + str(angles[3]) + " Y" + str((angles[4] + angles[5])) + \
             " Z" + str(round(-angles[4] + angles[5], 2))
+        messageToSend = message + "\n"
+        s0.write(messageToSend.encode('UTF-8'))
+        time.sleep(7)
+
+        # Third position change gripper
+        closing_percentage = 60
+        message = "M3 S" + str((255 / 20) * closing_percentage)
+        messageToSend = message + "\n"
+        s0.write(messageToSend.encode('UTF-8'))
+        time.sleep(7)
+
+        # Fourth position change gripper
+        closing_percentage = 20
+        message = "M3 S" + str((255 / 20) * closing_percentage)
         messageToSend = message + "\n"
         s0.write(messageToSend.encode('UTF-8'))
         time.sleep(7)
